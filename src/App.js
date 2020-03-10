018bc97fbc0aa78b5df07407da646a499baaddcf
Akbar Hirani <akbar.hirani123@gmail.com>
2020-03-09 22:55:44 -0400

    fake tweet message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Hello from './sayHello';
+import Tweet from './tweet';
 
 function App() {
   return (
-    <div>
-      <h1>This is the app component</h1>
-      <Hello />
+    <div className="app">
+      <Tweet name="Akbar" message="this is a random tweet"/>
+      <Tweet name="Jon Snow" message="I am the true king"/>
+      <Tweet name="Winter is Comming" message="And its gone..."/>
+      <Tweet name="Ghost" message="Woof"/>
     </div>
   );
 }
