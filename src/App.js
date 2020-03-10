4b232f02d8f7fca81985ad59dc94349cacac956f
Akbar Hirani <akbar.hirani123@gmail.com>
2020-03-09 23:14:09 -0400

    fake tweet message using states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hello from './sayHello';
 import Tweet from './tweet';
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count +1);
+    setRed(!isRed);
+  };
+
+  const [users, setUser] = useState([
+    {name: 'Akbar', message: "hello there"},
+    {name: 'Jon Snow', message: "I am true king"},
+    {name: 'Ghost', message: "woof"}
+  ]);
+
   return (
     <div className="app">
-      <Tweet name="Akbar" message="this is a random tweet"/>
-      <Tweet name="Jon Snow" message="I am the true king"/>
-      <Tweet name="Winter is Comming" message="And its gone..."/>
-      <Tweet name="Ghost" message="Woof"/>
+      {/*<h1 className={isRed ? 'red' : ''}>Change My Color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>*/}d
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
